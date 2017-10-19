752fd24194cbe84956f9ad7ba675414aa23931dc
zcsd <zichun92@gmail.com>
2017-10-19 23:30:12 +0800

    get templete vector

--- ShapeMatching.py
+++ ShapeMatching.py
@@ -3,27 +3,89 @@ import cv2
 
 print "Shape Matching Using Fourier Descriptor"
 
-# Main loop
-imgray = cv2.imread("a2.bmp", 0)
-retvalth, imgthreshold = cv2.threshold(imgray, 50, 255, cv2.THRESH_BINARY)
-imgthresholdNot = cv2.bitwise_not(imgthreshold)
-#cv2.RETR_EXTERNAL,TREE
-imgcontours, contours, hierarchy = cv2.findContours(imgthresholdNot, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#print contours.size
-imgdrawContours = np.zeros((imgray.shape[0],imgray.shape[1], 3), np.uint8)
-cv2.drawContours(imgdrawContours, contours, -1, (255, 255, 255), 1)
+rect = (0, 0, 1, 1)
+ix, iy = -1, -1
+temSeleteFlag = False
+temReadyFlag = False
+matchOverFlag = False
+temConfirmFlag = False
+templeteVector = []
 
-for contour in contours:
-    (x, y), radius = cv2.minEnclosingCircle(contour)
-    center = (int(x), int(y))
-    radius = int(radius)
-    cv2.circle(imgdrawContours, center, radius, (0,255,0), 1)
+def selectTemplete(event, x, y, flags, param):
+    global rect, temSeleteFlag, temReadyFlag, ix, iy
+
+    if event == cv2.EVENT_LBUTTONDOWN and temReadyFlag == False:
+        temSeleteFlag = True
+        ix, iy = x, y
+       
+    elif event == cv2.EVENT_LBUTTONUP:
+        if temReadyFlag == False and temSeleteFlag == True:
+            rect = (min(ix,x), min(iy,y), abs(ix-x), abs(iy-y))
+            cv2.rectangle(imgOri, (ix,iy), (x,y), (255,0,0), 2)
+        temSeleteFlag = False
+        temReadyFlag = True
+
+def getContours(img):
+    imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    retvalth, imgthreshold = cv2.threshold(imgray, 50, 255, cv2.THRESH_BINARY)
+    imgthresholdNot = cv2.bitwise_not(imgthreshold)
+    kernel = np.ones((5,5), np.uint8)
+    imgdilation = cv2.dilate(imgthresholdNot, kernel, iterations=2)
+    #cv2.RETR_EXTERNAL,TREE
+    imgcontours, contours, hierarchy = cv2.findContours(imgdilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    #print contours.size
+    imgdrawContours = np.zeros((imgray.shape[0],imgray.shape[1], 3), np.uint8)
+    cv2.drawContours(imgdrawContours, contours, -1, (255, 255, 255), 1)
+
+    #print len(contours)
+                
+    #cv2.imshow("Original Gray", imgray)
+    #cv2.imshow("Threshold", imgthreshold)
+    #cv2.imshow("Contours", imgcontours)
+    #imgcontourShow = cv2.resize(imgdrawContours, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+    #cv2.imshow("drawcontours", imgcontourShow)
+    return contours
+
+def getTempleteCV():
+    templeteROI = imgOricpy[rect[1]:rect[1]+rect[3], rect[0]:rect[0]+rect[2]]
+    tpContour = getContours(templeteROI)
+
+    for cnt in tpContour:
+        x, y, w, h = cv2.boundingRect(cnt)
+        for point in cnt:
+            templeteVector.append( (point[0][0]-x, point[0][1]-y) )
+
+def getSampleCV():
+    pass
+                           
+# Main loop
+imgOri = cv2.imread("a2.bmp", 1)
+imgOricpy = imgOri.copy()
+cv2.namedWindow("Original Image")
+cv2.setMouseCallback("Original Image", selectTemplete)
+
+while(True):
+    cv2.imshow("Original Image", imgOri)
     
-#cv2.imshow("Original Gray", imgray)
-#cv2.imshow("Threshold", imgthreshold)
-#cv2.imshow("Contours", imgcontours)
-imgcontourShow = cv2.resize(imgdrawContours, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
-cv2.imshow("drawcontours", imgcontourShow)
-cv2.waitKey(0)
+    if temReadyFlag == True and matchOverFlag == False and temConfirmFlag == True:
+        getTempleteCV();
+        contours = getContours(imgOricpy)
+        cv2.drawContours(imgOri, contours, -1, (0, 0, 255), 1)
+        for contour in contours:
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.rectangle(imgOri, (x,y), (x+w,y+h), (0,255,0), 1)
+            #rect = cv2.minAreaRect(contour)
+            #box = cv2.boxPoints(rect)
+            #box = np.int0(box)
+            #cv2.drawContours(imgOri, [box],0,(0,255,0),1)
+            
+        matchOverFlag = True
+        
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('y') or key == ord('Y'):
+        temConfirmFlag = True
+    elif key == ord('q') or key == ord('Q'):    
+        break
+ 
 cv2.destroyAllWindows()
 
